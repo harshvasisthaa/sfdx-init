b67cef3e80e23d501e1c48eab98a2878ba61d4e2
harshvasisthaa <harshvasisthaa@gmail.com>
2022-10-02 20:24:20 +0530

    Update deploy.cmd

--- deploy.cmd
+++ deploy.cmd
@@ -6,7 +6,8 @@ set SFDC_PROD_USER="harshvijaykumarrawat@gmail.com"
 set SFDC_SANDBOX_USER="harshvijaykumarrawat@gmail.com"
 
 set SANDBOX_CONSUMER_KEY="testconkey"
-set PROD_CONSUMER_KEY="testconkey"
+set PROD_CONSUMER_KEY="3MVG9fe4g9fhX0E6kcopvWInnuBJxpZc1Niht_PenbEv4hgc3_.nYiM3dPSbqk7lq3JC2i0iecHSLHHjiBYkh"
+set PROD_CONSUMER_SEC="A2AB75CA3AA396F5918181D216640A99F831FDB02DFC432F34E4E10B87FF1B92"
 
 set GIT_REPO_LOCATION="test/location/"
 
@@ -14,6 +15,6 @@ echo %SFDC_PROD_URL%
 echo %SFDC_PROD_USER%
 
 
-sfdx force:auth:jwt:grant --clientid %CONSUMER_KEY% --username %SFDC_PROD_USER% --jwtkeyfile keys/server.key --setdefaultdevhubusername --setalias sfdx-ci --instanceurl %SFDC_PROD_URL%
+sfdx force:auth:jwt:grant --clientid %CONSUMER_KEY% --username %SFDC_PROD_USER% --jwtkeyfile key/server.key --setdefaultdevhubusername --setalias sfdx-ci --instanceurl %SFDC_PROD_URL%
 sfdx force:source:convert -d mdapi
 sfdx force:deploy -p %SOURCE_PATH% -u %SFDC_PROD_USER%
